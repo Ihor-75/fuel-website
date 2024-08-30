bfab312a2b96b756dc3f2159e6ecf9a51d1fdf6f
Ihor-75 <deneha97ihor@ukr.net>
2024-08-30 16:28:16 +0300

    Add styles

--- src/app/statements/statements.component.ts
+++ src/app/statements/statements.component.ts
@@ -1,12 +1,15 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-statements',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './statements.component.html',
   styleUrl: './statements.component.scss'
 })
 export class StatementsComponent {
+
+  allImages: boolean = true;
 
 }
